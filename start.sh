6a5a8b4491f6f41cfdf863adee3f3612a2d7c877
DeusMaximus <DeusMaximus@users.noreply.github.com>
2018-05-22 15:30:46 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /mnt/user/data/data /data/logs
+mkdir -p /mnt/user/data/data mnt/user/data/logs
 docker rm -f cache
 
 docker run --name cache -d --restart=always -p 80:80 -v /mnt/user/data/data:/cache -v /mnt/user/data/logs:/var/log/nginx cache
